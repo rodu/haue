50f8e86e2897e936b26a9a0635cb572ab8263c56
rodu <greenheron35@gmail.com>
2016-12-09 07:02:11 +0000

    Enables binding active class on selected group

--- src/components/hue-groups/hue-groups.js
+++ src/components/hue-groups/hue-groups.js
@@ -1,25 +1,36 @@
 import { inject } from 'aurelia-framework';
 import { LogManager } from 'aurelia-framework';
-import _ from 'lodash/lodash';
 import { Store } from 'aurelia-redux-plugin';
-
+import _ from 'lodash';
 import { HueGroupsService } from '../../services/HueGroupsService';
 import groupSelectedAction from '../../actions/groupSelectedAction';
+import StateReader from '../../services/StateReader';
+
 const logger = LogManager.getLogger('HueGroups');
 
-@inject(HueGroupsService, Store)
+const mapActiveGroup = function(group) {
+  group.active = (this.activeGroupName === group.name) ? 'active' : '';
+  return group;
+};
+
+@inject(HueGroupsService, Store, StateReader)
 export class HueGroups {
 
   groups = [];
 
-  constructor(hueGroupsService, store) {
+  constructor(hueGroupsService, store, stateReader) {
     this.hueGroupsService = hueGroupsService;
     this.store = store;
+    this.stateReader = stateReader;
   }
 
   created() {
     this.hueGroupsService.getGroups().then((groups) => {
-      this.groups = _.map(groups, 'name');
+      const mapBinding = {
+        activeGroupName: this.stateReader.getProp('groupSelected.name')
+      };
+
+      this.groups = _.map(groups, _.bind(mapActiveGroup, mapBinding));
     }).catch((error) => {
       logger.error(error.message);
     });
