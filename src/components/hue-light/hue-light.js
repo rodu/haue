243b2879e20f7a862eaf5962063f2c14f2099fee
rodu <greenheron35@gmail.com>
2016-12-10 12:56:21 +0000

    Renders light colors converting from HSL to RGB

--- src/components/hue-light/hue-light.js
+++ src/components/hue-light/hue-light.js
@@ -1,10 +1,17 @@
-import { bindable } from 'aurelia-framework';
+import { bindable, LogManager } from 'aurelia-framework';
+
+
+const logger = LogManager.getLogger('HueLight');
 
 export class HueLight {
   @bindable light;
 
   bind() {
-    console.log(this.light);
+    logger.info('Binding light', this.light);
 
+    const rgb = this.light.state.rgb;
+    this.lightStyle = {
+      'background-color': `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+    };
   }
 }
--- src/services/HueLightsService.js
+++ src/services/HueLightsService.js
@@ -1,5 +1,15 @@
 import { inject } from 'aurelia-framework';
 import { HueService } from './HueService';
+import hsl2rgb from 'hsl-to-rgb';
+
+const convertHSLtoRGB = (light) => {
+  const state = light.state;
+  const xy = state.xy;
+
+  state.rgb = hsl2rgb(state.bri, xy[0], xy[1]);
+
+  return light;
+};
 
 @inject(HueService)
 export class HueLightsService {
@@ -11,7 +21,7 @@ export class HueLightsService {
   getLights() {
     return new Promise((resolve, reject) => {
       this.bridge.getLights((lights) => {
-        resolve(Object.values(lights));
+        resolve(Object.values(lights).map(convertHSLtoRGB));
       }, (error) => {
         reject(new Error('Could not get groups from bridge!'));
       });
