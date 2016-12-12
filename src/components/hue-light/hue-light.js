f590c951a54c23b66e97cfc79e166bd13dd32698
rodu <greenheron35@gmail.com>
2016-12-12 07:22:10 +0000

    Shows the percentage brightness for the lights

--- src/components/hue-light/hue-light.js
+++ src/components/hue-light/hue-light.js
@@ -2,6 +2,17 @@ import { bindable, LogManager } from 'aurelia-framework';
 
 
 const logger = LogManager.getLogger('HueLight');
+const getBackgroundColor = (state, brightness) => {
+  if (state.on) {
+    const rgb = state.rgb;
+    const value = `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${brightness})`;
+
+    logger.info(value);
+    return value;
+  }
+
+  return '#ccc';
+};
 
 export class HueLight {
   @bindable light;
@@ -10,12 +21,11 @@ export class HueLight {
     logger.info('Binding light', this.light);
 
     const state = this.light.state;
-    const rgb = state.rgb;
-    const bri = (state.bri / 255).toFixed(1);
+    const brightness = (state.bri / 255).toFixed(1);
 
-    logger.info(`rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${bri})`);
     this.lightStyle = {
-      'background-color': `rgba(${rgb[0]}, ${rgb[1]}, ${rgb[2]}, ${bri})`
+      'background-color': getBackgroundColor(state, brightness)
     };
+    this.percentBrightness = Math.round(brightness * 100) + '%';
   }
 }
